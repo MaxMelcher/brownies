cbb6167dbf7bd4a631eb8025d6fa866aada67697
Stina <stinaqv@gmail.com>
2013-08-14 14:57:59 +0200

    it wanted quiet

--- Safari/tools/ChocolateyInstall.ps1
+++ Safari/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'safari' 'exe' 'silent' 'http://appldnld.apple.com/Safari5/041-5487.20120509.INU8B/SafariSetup.exe'
+Install-ChocolateyPackage 'safari' 'exe' '/quiet' 'http://appldnld.apple.com/Safari5/041-5487.20120509.INU8B/SafariSetup.exe'
