db28c7ab7ebd2ac51576ce4d690fb552beb5a1d7
Stina <stinaqv@gmail.com>
2013-08-14 15:04:19 +0200

    had to change id, cause someone had stolen it

--- Safari/tools/ChocolateyInstall.ps1
+++ Safari/tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'safari' 'exe' '/quiet' 'http://appldnld.apple.com/Safari5/041-5487.20120509.INU8B/SafariSetup.exe'
+Install-ChocolateyPackage 'safariwindows' 'exe' '/quiet' 'http://appldnld.apple.com/Safari5/041-5487.20120509.INU8B/SafariSetup.exe'
