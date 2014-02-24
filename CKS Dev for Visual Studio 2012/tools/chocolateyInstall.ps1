57bf4d187af9021a33a2cd3874d7dc78338c8f35
MaxMelcher <melcher.maximilian@gmail.com>
2014-02-24 11:31:28 +0100

    Upgrade to CKSDEV 1.2
    Simplified the installer

--- CKS Dev for Visual Studio 2012/tools/chocolateyInstall.ps1
+++ CKS Dev for Visual Studio 2012/tools/chocolateyInstall.ps1
@@ -1,46 +1,12 @@
-$packageName = 'CKS - Dev for Visual Studio 2012'
-
-function Get-Batchfile ($file) {
-  $cmd = "`"$file`" & set"
-    cmd /c $cmd | Foreach-Object {
-      $p, $v = $_.split('=')
-        Set-Item -path env:$p -value $v
-    }
-}
-
-function VsVars32() {
-  if (!(Test-Path env:VS110COMNTOOLS)) {
-    throw "Visual Studio could not be found on this computer. You need to have it installed to install this plugin"
-  } else {
-    $BatchFile = join-path $env:VS110COMNTOOLS "vsvars32.bat"
-    Get-Batchfile `"$BatchFile`"
-  }
-}
-
-function curlex($url, $filename) {
-  $path = [io.path]::gettemppath() + "\" + $filename
-
-    if( test-path $path ) { rm -force $path }
-
-  (new-object net.webclient).DownloadFile($url, $path)
-
-    return new-object io.fileinfo $path
-}
-
-function installsilently($url, $name) {
-  echo "Installing $name"
-
-  $extension = (curlex $url $name).FullName
-
-  $result = Start-Process -FilePath "VSIXInstaller.exe" -ArgumentList "/q $extension" -Wait -PassThru;
-}
-
-try {
-  VsVars32
-  installsilently http://visualstudiogallery.msdn.microsoft.com/cf1225b4-aa83-4282-b4c6-34feec8fc5ec/file/96582/3/CKS.Dev11.vsix CKS.Dev11.vsix
-
-   Write-ChocolateySuccess $packageName
+try
+{
+  Install-ChocolateyVsixPackage 'CKS - Dev for Visual Studio 2012 and 2013' http://visualstudiogallery.msdn.microsoft.com/cf1225b4-aa83-4282-b4c6-34feec8fc5ec/file/96582/4/CKS.Dev11.vsix
+  Write-ChocolateySuccess 'CKS - Dev for Visual Studio 2012 and 2013'
 } catch {
-  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-  throw
+  Write-ChocolateyFailure 'CKS - Dev for Visual Studio 2012 and 2013' "$($_.Exception.Message)"
+  throw 
 }
+
+
+
+
